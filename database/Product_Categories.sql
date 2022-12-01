ab6bfedb1133f641229aef98f290efeae8a14eec
Zeta <matiaszelazny99@gmail.com>
2022-12-01 14:17:27 -0300

    Se agrega campo ID_Product_Category

--- database/Product_Categories.sql
+++ database/Product_Categories.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS public.product_categories
 (
+    id_product_category character(5) NOT NULL,
     id_category integer NOT NULL,
     category character(20) COLLATE pg_catalog."default",
     id_subcategory integer NOT NULL,
     subcategory character(20) COLLATE pg_catalog."default",
-    CONSTRAINT product_categories_pkey PRIMARY KEY (id_category, id_subcategory)
+    CONSTRAINT product_categories_pkey PRIMARY KEY (id_product_category)
 )
