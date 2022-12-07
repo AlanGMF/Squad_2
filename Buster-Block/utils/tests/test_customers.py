05d78292e6a89bf0b15b03074fd8a9f7d35c2d0b
SabrinaBenitez <slbprofesional@gmail.com>
2022-12-07 14:55:13 -0300

    Modificacion a funcion customers

--- Buster-Block/utils/tests/test_customers.py
+++ Buster-Block/utils/tests/test_customers.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+from pandas.testing import assert_frame_equal
 import sys
 import os
 from pathlib import Path
@@ -11,20 +12,19 @@ from etl.transform import df_customers
 
 class TestCustomers(unittest.TestCase):
     
-    def tests_customer(self):
-        
-        filepath = "/mnt/c/Users/Alan Hernandez/Desktop/Squad_2/BusterBlock/utils/tests/test_data/raw_data/customers.html"
+    def tests_customer1(self):
+        """Test "df_customer" function
+        """
        # Split the extension from the path
         try:
+            filepath = "/mnt/c/Users/Alan Hernandez/Desktop/Squad_2/BusterBlock/utils/tests/test_data/raw_data/customer.csv"
             ext = os.path.splitext(filepath)[-1]
             df_customers(filepath) 
             self.assertEqual(ext,".csv")
-        except:
-            ext = "Invalid extension"
-            assert ext == "Invalid extension"   
-        
-         
           
-            
+        except AssertionError:
+            ext = "Invalid extension"
+            assert ext == "Invalid extension"    
+                    
 if __name__ == '__main__':
     unittest.main()
