d836fe6f3bce0289c7d0c318cf9398475a7658b2
alan <alan.g.melofreyre@gmail.com>
2022-12-14 20:28:13 -0300

    Add the feature that the endpoint runs the ETL and modify the function to reuse it

--- Buster-Block/api/utils/function.py
+++ Buster-Block/api/utils/function.py
@@ -1,23 +1,26 @@
 import os
 from pathlib import Path
 DIR = os.path.dirname(os.path.normpath(__file__)).rstrip('/api/utils')
-path_utils = Path(__file__).parents[2].joinpath("utils").joinpath("etl").joinpath("processed_data")
+PATH_PROCESS_DATA = Path(__file__).parents[2].joinpath("utils").joinpath("etl").joinpath("processed_data")
+PATH_RAW_DATA = Path(__file__).parents[2].joinpath("utils").joinpath("etl").joinpath("RAW_DATA")
 
-def get_csv_files() -> list:
+def get_csv_files(path) -> list:
     """this function creates a list with the paths of the files
 
+    :param path: file path to get name files
+    :type path: str
     :return: the paths list
     :rtype: list
     """
     output_dir = f'{DIR}/utils/etl/processed_data/'
-    rootDir = path_utils
-    print(rootDir)
+    rootDir = path
+
     for dirName, subdirList, fileList in os.walk(rootDir):
         filenames = []
         for fname in fileList:
             # append file path
-            file_path = str(path_utils.joinpath(fname))
+            file_path = str(path.joinpath(fname))
             filenames.append(file_path)
             # filenames.append(output_dir + fname)
-    print(filenames)
+
     return filenames
