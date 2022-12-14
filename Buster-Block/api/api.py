bf61399257987c10ddfe959e2a666a21ed30e47c
alan <alan.g.melofreyre@gmail.com>
2022-12-14 10:56:15 -0300

    add changes to make it run on windows

--- Buster-Block/api/api.py
+++ Buster-Block/api/api.py
@@ -188,4 +188,4 @@ async def transform_save_data(
 
 # main to run fastapi automatically
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- Buster-Block/api/utils/function.py
+++ Buster-Block/api/utils/function.py
@@ -1,7 +1,7 @@
 import os
-
+from pathlib import Path
 DIR = os.path.dirname(os.path.normpath(__file__)).rstrip('/api/utils')
-
+path_utils = Path(__file__).parents[2].joinpath("utils").joinpath("etl").joinpath("processed_data")
 
 def get_csv_files() -> list:
     """this function creates a list with the paths of the files
@@ -10,11 +10,14 @@ def get_csv_files() -> list:
     :rtype: list
     """
     output_dir = f'{DIR}/utils/etl/processed_data/'
-    rootDir = output_dir
-
+    rootDir = path_utils
+    print(rootDir)
     for dirName, subdirList, fileList in os.walk(rootDir):
         filenames = []
         for fname in fileList:
             # append file path
-            filenames.append(output_dir + fname)
+            file_path = str(path_utils.joinpath(fname))
+            filenames.append(file_path)
+            # filenames.append(output_dir + fname)
+    print(filenames)
     return filenames
